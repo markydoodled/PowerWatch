692971a42fc693ff93e92f6c3497c388cd83a5f0
Mark Howard <markhoward2005@gmail.com>
2023-08-03 22:25:59 +0100

    Added Base iOS Project

---
+++ Source Code/PowerWatch/PowerWatch/PowerWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PowerWatchApp.swift
+//  PowerWatch
+//
+//  Created by Mark Howard on 02/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct PowerWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
