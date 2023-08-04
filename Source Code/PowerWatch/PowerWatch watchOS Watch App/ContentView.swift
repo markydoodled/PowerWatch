fa3a168be4d6eb9b8d65f92ba6629fa0f435c06a
Mark Howard <markhoward2005@gmail.com>
2023-08-04 17:57:57 +0100

    Added watchOS Project Target

---
+++ Source Code/PowerWatch/PowerWatch watchOS Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PowerWatch watchOS Watch App
+//
+//  Created by Mark Howard on 04/08/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Source Code/PowerWatch/PowerWatch watchOS Watch App/PowerWatch_watchOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PowerWatch_watchOSApp.swift
+//  PowerWatch watchOS Watch App
+//
+//  Created by Mark Howard on 04/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct PowerWatch_watchOS_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
